0cc350f0a078bb39c67fea3f2c89e1a5a39212e9
joshuadaquila <138364264+joshuadaquila@users.noreply.github.com>
2024-07-18 16:57:00 +0800

    Update db.js

--- dependency/db.js
+++ dependency/db.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql');
 
-// Configure MySQL connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'alumniHubDb'
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-module.exports = db;
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database');
+});
