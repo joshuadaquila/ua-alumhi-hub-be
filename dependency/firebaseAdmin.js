a9d0a70b7a9927d69acdcbe22a1ef3a04358a12b
DESKTOP-F2KH5J3\ojt <joshua.daquila@antiquespride.edu.ph>
2024-07-31 14:04:13 +0800

    Firebase config

--- dependency/firebaseAdmin.js
+++ dependency/firebaseAdmin.js
@@ -1,14 +1,22 @@
 const admin = require('firebase-admin');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
-const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const serviceAccountBase64 = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-if (!serviceAccountPath) {
+if (!serviceAccountBase64) {
   throw new Error('Missing the GOOGLE_APPLICATION_CREDENTIALS environment variable');
 }
 
-const serviceAccount = require(path.resolve(serviceAccountPath));
+// Decode the base64-encoded service account key
+const serviceAccountJson = Buffer.from(serviceAccountBase64, 'base64').toString('utf8');
+
+// Write the JSON to a temporary file
+const tempFilePath = path.join(__dirname, 'temp-serviceAccountKey.json');
+fs.writeFileSync(tempFilePath, serviceAccountJson);
+
+const serviceAccount = require(tempFilePath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,3 +25,8 @@ admin.initializeApp({
 
 const bucket = admin.storage().bucket();
 module.exports = { bucket };
+
+// Clean up the temporary file if necessary
+process.on('exit', () => {
+  fs.unlinkSync(tempFilePath);
+});
