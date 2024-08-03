8c648afe98f824afd47dbcaba3d5e5e86e88aaee
joshuadaquila <joshuanomongdaquila@gmail.com>
2024-08-03 22:41:56 +0800

    Update socket.io

--- dependency/socketio.js
+++ dependency/socketio.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
+import db from './db';
 
 const app = express();
 const server = createServer(app);
@@ -18,18 +19,56 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-  // console.log('a user connected hehe'); //sends to client
-  // const msg = "guys may nag connect";
-  // socket.emit('chika', msg);
+  console.log('A user connected');
 
-  socket.on('eventNotification', (msg) => { //receives from client
-    console.log('message: ' + msg);
-    io.emit('eventNotification', msg); //send message to all connected
+  socket.on('eventNotification', (msg) => {
+    console.log('Event message:', msg);
+    io.emit('eventNotification', msg); // Send event notification to all connected clients
   });
 
-  socket.on('messageNotification', (msg) => { //receives from client
-    console.log('message: ' + msg);
-    io.emit('messageNotification', msg); //send message to all connected
+  socket.on('messageNotification', (msg) => {
+    console.log('Received message:', msg);
+
+    // Query to get message details
+    const getMessageQuery = 'SELECT * FROM message WHERE messageid = ?';
+    db.query(getMessageQuery, [msg.insertId], (err, messageResults) => {
+      if (err) {
+        console.error('Error fetching message:', err);
+        return;
+      }
+
+      if (messageResults.length > 0) {
+        const message = messageResults[0];
+        const userId = message.userid;
+
+        // Query to get user details
+        const getUserQuery = 'SELECT * FROM alumni WHERE alumniid = ?';
+        db.query(getUserQuery, [userId], (err, userResults) => {
+          if (err) {
+            console.error('Error fetching user details:', err);
+            return;
+          }
+
+          if (userResults.length > 0) {
+            const user = userResults[0];
+            const enrichedMessage = {
+              ...message,
+              name: user.name,
+              email: user.email,
+              photourl: user.photourl
+            };
+
+            io.emit('messageNotification', enrichedMessage); // Send enriched message to all connected clients
+          } else {
+            // Handle case where user is not found
+            io.emit('messageNotification', message); // Send original message if user details are not found
+          }
+        });
+      } else {
+        // Handle case where message is not found
+        console.error('Message not found');
+      }
+    });
   });
 });
 
