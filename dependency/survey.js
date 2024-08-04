6bcdfe83fff11e5d882cfdba900c435009574893
joshuadaquila <joshuanomongdaquila@gmail.com>
2024-08-04 21:10:49 +0800

    updated tracer survey api

--- dependency/survey.js
+++ dependency/survey.js
@@ -2,30 +2,26 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db');
 
-router.post('/submitSurvey', (req, res) => {
+router.post('/submitGeneralInfo', (req, res) => {
   const userId = req.userId;
-  const { contactNum, mobileNum, civilStat, sex, region, province, residence,
-    educationalAttain, professionalExams, undergraduateReasons, graduateReasons,
-    trainings, reasonAdvanceStud, presentlyEmployed, reasonUnemployed, presentEmployStat, skillsAcquired, presentOccupation, majorLine, placeOfWork, firstJobAfterJob, 
-    reasonStayingJob, firstJobRelatedToCourse, reasonAcceptingJob, reasonChangingJob, durationFirstJob, howFindFirstJob, durationJobSeeking, firstJobLvl,
-    secondJobLvl, earning, curriculumRelevance, competencies, suggestion, 
-    awardName, awardBody, date, selectedFiles } = req.body;
+  const { contactNum, mobileNum, civilStat, sex, region, province, residence } = req.body;
 
-  const queries = [];
-
-  // General Information Query
   const generalInfoSql = `INSERT INTO generalinformation 
   (alumniid, telnumber, mobilenum, civilstatus, sex, region, province, residence) VALUES (?,?,?,?,?,?,?,?)`;
-  queries.push(new Promise((resolve, reject) => {
-    db.query(generalInfoSql, [userId, contactNum, mobileNum, civilStat, sex, region, province, residence], (err, result) => {
-      if (err) {
-        return reject(new Error('GENINFO: ' + err.message));
-      }
-      resolve(result);
-    });
-  }));
+  
+  db.query(generalInfoSql, [userId, contactNum, mobileNum, civilStat, sex, region, province, residence], (err, result) => {
+    if (err) {
+      console.error('GENINFO: ' + err.message);
+      return res.status(500).send(err.message);
+    }
+    res.json({ message: 'General information inserted successfully', result });
+  });
+});
 
-  // Educational Background Query
+router.post('/submitEducationalBackground', (req, res) => {
+  const userId = req.userId;
+  const { educationalAttain, professionalExams, undergraduateReasons, graduateReasons } = req.body;
+
   const educAttainJson = JSON.stringify(educationalAttain);
   const profExamJson = JSON.stringify(professionalExams);
   const undergradReasJson = JSON.stringify(undergraduateReasons);
@@ -33,31 +29,41 @@ router.post('/submitSurvey', (req, res) => {
 
   const educationalBackgroundSql = `INSERT INTO educationalbackground 
   (alumniid, educattain, exampassed, reasonundergrad, reasongrad) VALUES (?,?,?,?,?)`;
-  queries.push(new Promise((resolve, reject) => {
-    db.query(educationalBackgroundSql, [userId, educAttainJson, profExamJson, undergradReasJson, gradReasJson], (err, result) => {
-      if (err) {
-        return reject(new Error('EDUC: ' + err.message));
-      }
-      resolve(result);
-    });
-  }));
+  
+  db.query(educationalBackgroundSql, [userId, educAttainJson, profExamJson, undergradReasJson, gradReasJson], (err, result) => {
+    if (err) {
+      console.error('EDUC: ' + err.message);
+      return res.status(500).send(err.message);
+    }
+    res.json({ message: 'Educational background inserted successfully', result });
+  });
+});
 
-  // Training Query
+router.post('/submitTraining', (req, res) => {
+  const userId = req.userId;
+  const { trainings, reasonAdvanceStud } = req.body;
+
   const trainingJson = JSON.stringify(trainings);
   const reasAdvanStudJson = JSON.stringify(reasonAdvanceStud);
 
   const trainingSql = `INSERT INTO training 
   (alumniid, trainingtitle, reason) VALUES (?,?,?)`;
-  queries.push(new Promise((resolve, reject) => {
-    db.query(trainingSql, [userId, trainingJson, reasAdvanStudJson], (err, result) => {
-      if (err) {
-        return reject(new Error('TRAINING: ' + err.message));
-      }
-      resolve(result);
-    });
-  }));
+  
+  db.query(trainingSql, [userId, trainingJson, reasAdvanStudJson], (err, result) => {
+    if (err) {
+      console.error('TRAINING: ' + err.message);
+      return res.status(500).send(err.message);
+    }
+    res.json({ message: 'Training data inserted successfully', result });
+  });
+});
 
-  // Employment Data Query
+router.post('/submitEmploymentData', (req, res) => {
+  const userId = req.userId;
+  const { presentlyEmployed, reasonUnemployed, presentEmployStat, skillsAcquired, presentOccupation, majorLine, placeOfWork, firstJobAfterJob, 
+    reasonStayingJob, firstJobRelatedToCourse, reasonAcceptingJob, reasonChangingJob, durationFirstJob, howFindFirstJob, durationJobSeeking, firstJobLvl,
+    secondJobLvl, earning, curriculumRelevance, competencies, suggestion } = req.body;
+
   const reasUnemJson = JSON.stringify(reasonUnemployed);
   const reasStayJson = JSON.stringify(reasonStayingJob);
   const reasAccJson = JSON.stringify(reasonAcceptingJob);
@@ -70,40 +76,34 @@ router.post('/submitSurvey', (req, res) => {
     presentoccupation, lineofbusiness, placeofwork, firstjob, reasonstayingonjob, firstjobrelatedtocourse,
     reasonacceptingthejob, reasonchangingjob, firstjobduration, howfoundfirstjob, howlongfoundfirstjob, joblvlposfirstjob, 
     joblvlposcurrentjob, firstjobearning, curriculumrelevance, competencies, suggestions) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
-  queries.push(new Promise((resolve, reject) => {
-    db.query(employmentDataSql, [userId, presentlyEmployed, reasUnemJson, presentEmployStat, skillsAcquired, presentOccupation, majorLine, placeOfWork,
-      firstJobAfterJob, reasStayJson, firstJobRelatedToCourse, reasAccJson, reasChanJson, durationFirstJob, howFindFJobJson, durationJobSeeking, 
-      firstJobLvl, secondJobLvl, earning, curriculumRelevance, compJson, suggestion], (err, result) => {
-      if (err) {
-        return reject(new Error('EMPLOY: ' + err.message));
-      }
-      resolve(result);
-    });
-  }));
+  
+  db.query(employmentDataSql, [userId, presentlyEmployed, reasUnemJson, presentEmployStat, skillsAcquired, presentOccupation, majorLine, placeOfWork,
+    firstJobAfterJob, reasStayJson, firstJobRelatedToCourse, reasAccJson, reasChanJson, durationFirstJob, howFindFJobJson, durationJobSeeking, 
+    firstJobLvl, secondJobLvl, earning, curriculumRelevance, compJson, suggestion], (err, result) => {
+    if (err) {
+      console.error('EMPLOY: ' + err.message);
+      return res.status(500).send(err.message);
+    }
+    res.json({ message: 'Employment data inserted successfully', result });
+  });
+});
 
-  // Contribution Profile Query
+router.post('/submitContributionProfile', (req, res) => {
+  const userId = req.userId;
+  const { awardName, awardBody, date, selectedFiles } = req.body;
+
   const selectFilesJson = JSON.stringify(selectedFiles);
 
   const contributionProfileSql = `INSERT INTO contributionprofile 
   (alumniid, awardname, awardbody, date, certificate) VALUES (?,?,?,?,?)`;
-  queries.push(new Promise((resolve, reject) => {
-    db.query(contributionProfileSql, [userId, awardName, awardBody, date, selectFilesJson], (err, result) => {
-      if (err) {
-        return reject(new Error('CONTRI: ' + err.message));
-      }
-      resolve(result);
-    });
-  }));
-
-  // Execute all queries and handle response
-  Promise.all(queries)
-    .then(results => {
-      res.json({ message: 'All data inserted successfully', results });
-    })
-    .catch(error => {
-      console.error(error.message);
-      res.status(500).send(error.message);
-    });
+  
+  db.query(contributionProfileSql, [userId, awardName, awardBody, date, selectFilesJson], (err, result) => {
+    if (err) {
+      console.error('CONTRI: ' + err.message);
+      return res.status(500).send(err.message);
+    }
+    res.json({ message: 'Contribution profile inserted successfully', result });
+  });
 });
 
 module.exports = router;
