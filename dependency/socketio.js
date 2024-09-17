743ecf368810d442ae7934da4861c57df333d791
joshuadaquila <joshuanomongdaquila@gmail.com>
2024-09-17 21:24:47 +0800

    Update socketio.js

--- dependency/socketio.js
+++ dependency/socketio.js
@@ -73,7 +73,7 @@ io.on('connection', (socket) => {
                 "app_id": "9649e634-24e7-4692-bb25-c0fe5d33ce63", //9649e634-24e7-4692-bb25-c0fe5d33ce63
                 "headings": { "en": "New Message" },
                 "contents": { "en": message.content },
-                "exclude_subscription_ids": [msg.subId],
+                "excluded_segments": [msg.subId],
                 "included_segments": ["Total Subscriptions"],
                 "data": {}
               
