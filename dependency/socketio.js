6dc3d019046a641d28449deca11be2d85456ad2f
joshuadaquila <joshuanomongdaquila@gmail.com>
2024-09-17 09:40:03 +0800

    Update socketio.js

--- dependency/socketio.js
+++ dependency/socketio.js
@@ -72,8 +72,7 @@ io.on('connection', (socket) => {
             const notification = {
               headings: { en: 'New Message Received' },
               contents: { en: enrichedMessage.message },
-              // include_player_ids: [user.onesignal_player_id], // Target the recipient
-              // exclude_player_ids: [msg.senderPlayerId] // Exclude the sender (you need to include this ID in your message payload)
+              include_player_ids: [user.onesignal_player_id] // Use the OneSignal player ID of the user
             };
 
             client.createNotification(notification)
